d12f9e13d32e009cef2b22d46cde3cf1eae80e6b
RicardoRodriguezHernandez <aelpepe234@gmail.com>
2025-09-03 11:32:47 -0500

    Delete Unidad-1/Fundamentos_de_Programacion/programa_que_convierta_grados_Celsius_a_Fahrenheit directory

--- Unidad-1/Fundamentos_de_Programacion/programa_que_convierta_grados_Celsius_a_Fahrenheit/programa_que_convierta_grados_Celsius_a_Fahrenheit/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-double GradosCelsius, GradosFahrenheit;
-
-Console.Write("introduzca los grados celsius que desea convertir: ");
-GradosCelsius = double.Parse(Console.ReadLine());
-
-GradosFahrenheit = (GradosCelsius * 9 / 5) + 32;
-
-Console.WriteLine("Los Grados Fahrenheit: " + GradosFahrenheit);
