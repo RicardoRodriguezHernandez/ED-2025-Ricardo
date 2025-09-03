b2e934c2d915b9f4132ce704292dacd122b6bce1
RicardoRodriguezHernandez <aelpepe234@gmail.com>
2025-09-03 11:31:59 -0500

    Add files via upload

---
+++ Unidad-1/Fundamentos_de_Programacion/programa_que_convierta_grados_Celsius_a_Fahrenheit/programa_que_convierta_grados_Celsius_a_Fahrenheit/Program.cs
@@ -0,0 +1,9 @@
+﻿
+double GradosCelsius, GradosFahrenheit;
+
+Console.Write("introduzca los grados celsius que desea convertir: ");
+GradosCelsius = double.Parse(Console.ReadLine());
+
+GradosFahrenheit = (GradosCelsius * 9 / 5) + 32;
+
+Console.WriteLine("Los Grados Fahrenheit: " + GradosFahrenheit);
