7146aaa4cb64ec2a43a8737b92575d3351c994a1
RicardoRodriguezHernandez <aelpepe234@gmail.com>
2025-09-03 11:00:57 -0500

    Add files via upload

---
+++ Unidad-1/Estucturas de control/Programa que determine si un número es primo/Programa que determine si un número es primo/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int Numero;
+bool NumeroPrimo = true;
+
+Console.Write("Ingresa un numero: ");
+Numero = int.Parse(Console.ReadLine());
+
+if (Numero <= 1)
+{
+    NumeroPrimo = false;
+}
+else
+{
+    for (int i = 2; i <= Math.Sqrt(Numero); i++)
+    {
+        if (Numero % i == 0)
+        {
+            NumeroPrimo = false;
+            break;
+        }
+    }
+}
+if (NumeroPrimo == true)
+{
+    Console.WriteLine("Es un numero primo");
+}
+else
+{
+    Console.WriteLine("No es un numero primo");
+}
+Console.ReadKey();
