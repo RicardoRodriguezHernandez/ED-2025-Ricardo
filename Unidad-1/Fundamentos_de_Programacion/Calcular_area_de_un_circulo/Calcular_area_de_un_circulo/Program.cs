023019a466d2058c2886b4350582007b3a9e0f01
RicardoRodriguezHernandez <aelpepe234@gmail.com>
2025-09-03 11:41:35 -0500

    Add files via upload

---
+++ Unidad-1/Fundamentos_de_Programacion/Calcular_area_de_un_circulo/Calcular_area_de_un_circulo/Program.cs
@@ -0,0 +1,9 @@
+﻿
+double Radio, Area;
+
+Console.Write("Introduzca el radio del circulo: ");
+Radio = double.Parse(Console.ReadLine());
+
+Area = Math.PI * (Math.Pow(Radio,2));
+
+Console.WriteLine("El area del circulo es: " +  Area.ToString("F2"));
